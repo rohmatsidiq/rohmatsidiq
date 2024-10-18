bd32e4f86207fd5d84d99623cf1969fff4da8bd3
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-10-18 16:17:59 +0700

    update

--- app/items/SubTitle.jsx
+++ app/items/SubTitle.jsx
@@ -1,6 +1,6 @@
 export default function SubTitle({ children }) {
   return (
-    <h1 data-aos="zoom-in" className="text-[18px] font-bold">
+    <h1 data-aos="zoom-in" className="text-[18px] text-white font-bold">
       {children}
     </h1>
   );
--- app/items/Title.jsx
+++ app/items/Title.jsx
@@ -1,6 +1,6 @@
 export default function Title({ children }) {
   return (
-    <h1 data-aos="zoom-in" className="text-[32px] font-bold">
+    <h1 data-aos="zoom-in" className="text-[32px] text-white font-bold">
       {children}
     </h1>
   );
